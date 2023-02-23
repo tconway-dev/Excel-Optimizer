897a2cd55fbc48665931c51a266992805f057486
TJ Conway <83920543+tconway-dev@users.noreply.github.com>
2023-02-22 21:02:54 -0500

    Update Link-Finder.ps1
    Updated

--- Link Finder/Link-Finder.ps1
+++ Link Finder/Link-Finder.ps1
@@ -1,81 +1,95 @@
 #TMC 2/10/23
-#Rev 10 2/18/23
+#Rev 11 2/22/23
 #OOP Change 
-class ExcelFile {
-    [string] $Name
-    [string] $Path
-    [string[]] $LinkedWorksheets
-    [string[]] $LinkedWorkbooks
-    [string[]] $OleDbConnections
+class ExcelFileAnalyzer {
+    [System.Collections.ArrayList]$results
 
-    ExcelFile([string] $name, [string] $path) {
-        $this.Name = $name
-        $this.Path = $path
-        $this.LinkedWorksheets = @()
-        $this.LinkedWorkbooks = @()
-        $this.OleDbConnections = @()
+    ExcelFileAnalyzer() {
+        $this.results = New-Object System.Collections.ArrayList
     }
 
-    [void] AddLinkedWorksheet([string] $worksheet) {
-        $this.LinkedWorksheets += $worksheet
+    [void]AnalyzeDirectory([string]$path) {
+        $files = Get-ChildItem -Path $path -Include *.xls,*.xlsx -Recurse
+        foreach ($file in $files) {
+            $workbook = $this.OpenWorkbook($file.FullName, $true)
+            if ($workbook) {
+                $linkedWorksheets = $this.GetLinkedWorksheets($workbook)
+                $linkedWorkbooks = $this.GetLinkedWorkbooks($workbook)
+                $connections = $this.GetOleDbConnections($workbook)
+
+                if ($linkedWorksheets.Count -gt 0 -or $linkedWorkbooks.Count -gt 0 -or $connections.Count -gt 0) {
+                    $result = [PSCustomObject]@{
+                        FileName = $file.Name
+                        FilePath = $file.FullName
+                        LinkedWorksheets = $linkedWorksheets -join ";"
+                        LinkedWorkbooks = $linkedWorkbooks -join ";"
+                        Connections = $connections -join ";"
+                    }
+                    $this.results.Add($result) | Out-Null
+                }
+                $workbook.Close($false)
+            }
+        }
     }
 
-    [void] AddLinkedWorkbook([string] $workbook) {
-        $this.LinkedWorkbooks += $workbook
+    [Microsoft.Office.Interop.Excel.Workbook]OpenWorkbook([string]$path, [bool]$readOnly = $false) {
+        $excel = New-Object -ComObject Excel.Application
+        $excel.Visible = $false
+        $excel.DisplayAlerts = $false
+        $excel.AskToUpdateLinks = $false
+
+        if ($readOnly) {
+            $workbook = $excel.Workbooks.Open($path, 3, $true) # 3 = read-only mode
+        } else {
+            $workbook = $excel.Workbooks.Open($path)
+        }
+
+        if ($workbook -eq $null) {
+            Write-Error "Failed to open workbook: $path"
+            return $null
+        } else {
+            return $workbook
+        }
     }
 
-    [void] AddOleDbConnection([string] $name, [string] $connection) {
-        $this.OleDbConnections += "$name: $connection"
+    [string[]]GetLinkedWorksheets([Microsoft.Office.Interop.Excel.Workbook]$workbook) {
+        $links = $workbook.LinkSources([Microsoft.Office.Interop.Excel.XlLinkType]::xlExcelLinks)
+        if ($links) {
+            return $links | ForEach-Object { $workbook.LinkSources([Microsoft.Office.Interop.Excel.XlLinkType]::xlExcelLinks, $_) } | Select-Object -Unique
+        } else {
+            return @()
+        }
+    }
+
+    [string[]]GetLinkedWorkbooks([Microsoft.Office.Interop.Excel.Workbook]$workbook) {
+        $links = $workbook.LinkSources([Microsoft.Office.Interop.Excel.XlLinkType]::xlExcelLinks)
+        if ($links) {
+            return $links | ForEach-Object { Split-Path $_ } | Select-Object -Unique
+        } else {
+            return @()
+        }
+    }
+
+    [string[]]GetOleDbConnections([Microsoft.Office.Interop.Excel.Workbook]$workbook) {
+        $connections = @()
+        foreach ($connection in $workbook.Connections) {
+            $connectionStrings = $connection.OLEDBConnection.Connection
+            if ($connectionStrings -is [System.Array]) {
+                $connectionStrings | ForEach-Object { $connection.Name + ":" + $_ }
+            } else {
+                $connection.Name + ":" + $connectionStrings
+            }
+            $connections += $connectionStrings
+        }
+        return $connections
     }
 }
+   
+# Initialize the ExcelFileAnalyzer class
+$analyzer = New-Object ExcelFileAnalyzer
 
-class ExcelFileProcessor {
-    [Microsoft.Office.Interop.Excel.Application] $Excel
-    [string] $SearchPath
-    [string] $CSVFile
+# Analyze the directory and get the results
+$analyzer.AnalyzeDirectory("C:\path\to\directory")
 
-    ExcelFileProcessor([string] $searchPath, [string] $csvFile) {
-        # Validate input
-        if ([string]::IsNullOrWhiteSpace($searchPath)) {
-            throw "Search path cannot be empty or null."
-        }
-        if ([string]::IsNullOrWhiteSpace($csvFile)) {
-            throw "CSV file path cannot be empty or null."
-        }
-
-        # Initialize Excel application
-        $this.Excel = New-Object -ComObject Excel.Application
-        $this.Excel.Visible = $false
-
-        $this.SearchPath = $searchPath
-        $this.CSVFile = $csvFile
-    }
-
-    [ExcelFile] GetExcelFileInfo([string] $filePath) {
-        $file = New-Object ExcelFile -ArgumentList (Split-Path $filePath -Leaf), $filePath
-
-        try {
-            # Open Excel workbook
-            $workbook = $this.Excel.Workbooks.Open($filePath)
-
-            # Get linked worksheets
-            $linkedWorksheets = ($workbook.LinkSources([Microsoft.Office.Interop.Excel.XlLinkType]::xlExcelLinks)) | Select-Object -ExpandProperty Text
-            foreach ($linkedWorksheet in $linkedWorksheets) {
-                $file.AddLinkedWorksheet($linkedWorksheet)
-            }
-
-            # Get linked workbooks
-            $linkedWorkbooks = ($workbook.LinkSources([Microsoft.Office.Interop.Excel.XlLinkType]::xlExcelLinks)) | Select-Object -ExpandProperty FullName
-            foreach ($linkedWorkbook in $linkedWorkbooks) {
-                $file.AddLinkedWorkbook($linkedWorkbook)
-            }
-
-            # Get OLE DB connections
-            $oleDbConnections = $workbook.Connections
-            foreach ($oleDbConnection in $oleDbConnections) {
-                $file.AddOleDbConnection($oleDbConnection.Name, $oleDbConnection.Connection)
-            }
-        }
-        catch {
-            Write-Error "Error processing file: $($_.FullName)"
-        }
+# Output the results to a CSV file
+$analyzer.results | Export-Csv -Path "C:\path\to\output.csv" -NoTypeInformation
